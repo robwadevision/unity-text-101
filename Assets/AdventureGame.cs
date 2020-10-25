827b49827cec61d5f749f98ba7cf74e507bbf5ba
Rob Wade <rwadee14@gmail.com>
2020-10-25 18:08:29 +0000

    Arrays

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -8,6 +8,9 @@ public class AdventureGame : MonoBehaviour
     [ SerializeField] Text textComponent;
     [SerializeField] State startingState;
 
+    //int[] oddNumbers = { 1, 3, 5, 7, 9 };
+    //string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
     State state;
 
     // Start is called before the first frame update
@@ -15,6 +18,8 @@ public class AdventureGame : MonoBehaviour
     {
         state = startingState;
         textComponent.text = state.GetStateStory();
+        //Debug.Log(oddNumbers[3]);
+        //Debug.Log(daysOfWeek[1]);
     }
 
     // Update is called once per frame

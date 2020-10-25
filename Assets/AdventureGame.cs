906629712186e92928a1089fa4956337d2ca87d9
Rob Wade <rwadee14@gmail.com>
2020-10-25 16:43:02 +0000

    Update Text Component

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [ SerializeField] Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = "This is the first of many screens you will encounter.";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

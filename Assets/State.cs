3219097587e1e1b8fd01e8fe5488fb484b400121
Rob Wade <rwadee14@gmail.com>
2020-10-25 17:49:09 +0000

    Public Methods and Return Types

--- Assets/State.cs
+++ Assets/State.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "State")]
-
 public class State : ScriptableObject
 {
     [TextArea(10,14)] [SerializeField] string storyText;
+
+    public string GetStateStory()
+    {
+        return storyText;
+    }
 
 }

3491427a00ab476cebb3991376de37ece74f5c30
Rob Wade <rwadee14@gmail.com>
2020-10-26 22:47:52 +0000

    Version 1.0

--- Assets/Scripts/AdventureGame.cs
+++ Assets/Scripts/AdventureGame.cs
@@ -31,21 +31,16 @@ public class AdventureGame : MonoBehaviour
     private void ManageState()
     {
         var nextStates = state.GetNextStates();
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        for (int index = 0; index < nextStates.Length; index++)
         {
-            state = nextStates[0];
+            if (Input.GetKeyDown(KeyCode.Alpha1 + index))
+            {
+                state = nextStates[index];
+            }   
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
-            state = nextStates[1];
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            state = nextStates[2];
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            state = nextStates[3];
+            state = startingState;
         }
         textComponent.text = state.GetStateStory();
     }
